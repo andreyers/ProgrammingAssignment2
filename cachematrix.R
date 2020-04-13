5f27f311380bcecce0fb32ef333b88e89efc19c9
Andrea Villaroman <villaroman.andrea@gmail.com>
2020-04-13 15:47:07 -0700

    Made code look neater and edited some comment wording.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions saves computation time by setting a matrix object
+## with the capability to cache its inverse.
+## The first function creates the matrix object (a list of functions).
 
-## Write a short comment describing this function
+
+## This functions outputs a list of functions operating on an input matrix
+## The functions set and return the matrix & sets and caches its inverse when calculated.
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  s <- NULL              # initialize "s" (the inverse matrix)
+  set <- function(y) {   # fxn sets value of matrix
+      x <<- y
+      s <<- NULL
+  }
+  get <- function() x    # fxn returns value of matrix
+  setsolve <- function(solve) s <<- solve  # fxn sets inverse of matrix
+  getsolve <- function() s                 # fxn returns inverse matrix
 
+  list (set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## The second function calls the inverse matrix from the matrix object, if already calculated
+##  OR the function will calculate and set the inverse matrix if it hasn't already been calculated.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  s <- x$getsolve() # check if inverse is already calculated
+  
+  if(!is.null(s)) { # if inverse is already calculated, return s
+    message("getting cahced data")
+    return(s)
+  }
+  
+  # if statement does not run:
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
